54c406833329f81a2f5b86215cb9bb4c966e9c6c
AlvaroL <u202012770@upc.edu.pe>
2023-05-11 11:42:18 -0500

    test: typicode api host

--- src/app/services/offer.service.ts
+++ src/app/services/offer.service.ts
@@ -8,19 +8,22 @@ import { Observable } from 'rxjs';
 export class OfferService {
   constructor(private _http: HttpClient) { }
 
+  //'https://my-json-server.typicode.com/Haisha10/offers-app-u202012770'
+  url = 'http://localhost:3000';
+
   addOffer(data: any): Observable<any> {
-    return this._http.post('http://localhost:3000/artist', data);
+    return this._http.post(`${this.url}/artist`, data);
   }
 
   updateOffer(id: number, data: any): Observable<any> {
-    return this._http.put(`http://localhost:3000/artist/${id}`, data);
+    return this._http.put(`${this.url}/artist/${id}`, data);
   }
 
   getOfferList(): Observable<any> {
-    return this._http.get('http://localhost:3000/artist');
+    return this._http.get(`${this.url}/artist`);
   }
 
   deleteOffer(id: number): Observable<any> {
-    return this._http.delete(`http://localhost:3000/artist/${id}`);
+    return this._http.delete(`${this.url}/artist/${id}`);
   }
 }
